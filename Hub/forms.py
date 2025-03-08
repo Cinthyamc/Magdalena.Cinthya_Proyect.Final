f8aa20947ddc97663bbd97d5e3c09e19a7c7c397
Cinthya Magdalena Castillo <cinthya980912@gmail.com>
2025-03-07 21:19:26 -0300

    Primer commit limpio

---
+++ Hub/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Usuario
+
+'''class LogIn(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        field = ('user', 'fullname')
+        exclude = ['peo']'''
